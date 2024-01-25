bdd6733ee7bf9ce209119c5107463c5f4153c932
subash3472 <subash9791587351@gmail.com>
2024-01-25 22:26:24 +0530

    i did changes on header and footer , added some changes

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,23 +1,32 @@
 import { Link } from "react-router-dom";
 import Search from "./Search";
 
-export default function Header({cartItems}) {
-    return <nav className="navbar row">
-            <div className="col-12 col-md-3">
-                <div className="navbar-brand">
-                <Link to="/"> <img width="150px" src="/images/logo.png" /></Link>
-                </div>
-            </div>
+export default function Header({ cartItems }) {
+  return (
+    <nav className="navbar row">
+      <div className="col-12 col-md-3">
+        <div className="navbar-brand">
+          <Link to="/">
+            <h2 className="logo_name">SPcart</h2>
+          </Link>
+          {/* <h2 >SPcart</h2> */}
+        </div>
+      </div>
 
-            <div className="col-12 col-md-6 mt-2 mt-md-0">
-              <Search/>
-            </div>
+      <div className="col-12 col-md-6 mt-2 mt-md-0">
+        <Search />
+      </div>
 
-            <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
-                <Link to={"/cart"}>
-                    <span id="cart" className="ml-3">Cart</span>
-                    <span className="ml-1" id="cart_count">{cartItems.length}</span>
-                </Link>
-            </div>
-        </nav>
+      <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
+        <Link to={"/cart"}>
+          <span id="cart" className="ml-3">
+            Cart
+          </span>
+          <span className="ml-1" id="cart_count">
+            {cartItems.length}
+          </span>
+        </Link>
+      </div>
+    </nav>
+  );
 }
